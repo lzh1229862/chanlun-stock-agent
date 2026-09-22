9f15a1ab4c71636893fb8d723468ba335a55737a
lzh1229862 <2490588558@qq.com>
2026-09-22 11:13:31 +0800

    清理误提交的临时探针脚本 + gitignore 兜住 _tmp_*.py

--- _tmp_probe3.py
+++
@@ -1,5 +0,0 @@
-import sys, os
-sys.path.insert(0, os.getcwd())
-import akshare as ak
-ns = [x for x in dir(ak) if 'industry' in x or ('sector' in x) or x.startswith('sw_')]
-print('候选函数:'); [print('  ', x) for x in ns]
--- _tmp_probe5.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-sys.path.insert(0, os.getcwd())
-import akshare as ak
-df = ak.stock_sector_spot(indicator='新浪行业')
-print('板块数:', len(df), ' 覆盖股票数(合计):', int(df['公司家数'].sum()))
-print()
-names = list(zip(df['板块'], df['公司家数'], df['label']))
-for i in range(0, len(names), 3):
-    print('   ' + '   '.join('%-12s(%3d)' % (n, c) for n, c, l in names[i:i+3]))
